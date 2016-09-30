b5d1aeffba77d5c4494fb0b38a8bf01badd1eb72
Ngahu <jamaalaraheem@gmail.com>
2016-09-30 11:03:25 +0300

    adding of the index.html page and also the debuging of the app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, Response
-from camera import Camera
+#from camera import Camera
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index:
+def index():
     return render_template('index.html')
 
 def gen(camera):
@@ -19,5 +19,5 @@ def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(debug=True)
