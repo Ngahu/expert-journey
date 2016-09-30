62950980b07588f0d935ec9e01b8961568ceb7b6
Ngahu <jamaalaraheem@gmail.com>
2016-09-30 11:44:40 +0300

    adding of the reseting for the next frame

--- app/camera_pi.py
+++ app/camera_pi.py
@@ -27,6 +27,30 @@ class Camera(object):
         self.initialize()
         return self.frame
 
+    @classmethod
+    def _thread(cls):
+        with picamera.PiCamera() as camera:
+            #camera setup
+            camera.resolution = (320,240)
+            camera.hflip = True
+            camera.vflip = True
+
+            #the camera is warming up
+            camera.start_preview()
+            time.sleep(2)
+
+            stream= io.BytesIO()
+            for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True)
+
+                #store frame
+                stream.seek(0)
+                cls.frame = stream.read()
+
+
+                #reseting stream fr the next frame
+                stream.seek(0)
+                stream.truncate()
+
 
 
 
