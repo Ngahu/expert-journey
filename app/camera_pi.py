21f8897ab672e4634897c4b18a71b054fe54bf5a
Ngahu <jamaalaraheem@gmail.com>
2016-09-30 11:33:52 +0300

    adding of the function that get_frame

--- app/camera_pi.py
+++ app/camera_pi.py
@@ -11,10 +11,22 @@ class Camera(object):
 
 
 
-def initialize(self):
-    if camera.thread is None:
+    def initialize(self):
+        if camera.thread is None:
         #start background frame thread
         Camera.thread = threading.Thread(target=self.thread)
         Camera.thread.start()
+
+        #wait until frame turn to be available
+        while self.frame is None:
+            time.sleep(0)
+
+
+    def get_frame(self):
+        Camera.last_access = time.time()
+        self.initialize()
+        return self.frame
+
+
 
 
