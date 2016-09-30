78e70fcc3216b8f02a217fb418e0bd6a34d8ba96
Ngahu <jamaalaraheem@gmail.com>
2016-09-30 11:25:09 +0300

    adding of the camera_pi.py which controlls the pi camera

---
+++ app/camera_pi.py
@@ -0,0 +1,12 @@
+import time
+import io
+import picamera
+import threading
+
+
+class Camera(object):
+    thread = None  # background thread that reads frames from camera
+    frame = None  # current frame is stored here by background thread
+    last_access = 0  # time of last client access to the camera
+
+
