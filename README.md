4841870f55c2481d7ca00eb6f77a4a8073669a78
Ngahu <jamaalaraheem@gmail.com>
2016-10-10 09:58:54 +0300

    adding of the read me file that helps understn about the project

---
+++ README.md
@@ -0,0 +1,13 @@
+#FLask Video Streaming 
+This entails  streaming video from the Rasberry Pi camera to web browsers to android and even ios phones  using motion jpeg
+
+you need to install the following for the module to work:
+ click==6.6
+Flask==0.11.1
+itsdangerous==0.24
+Jinja2==2.8
+MarkupSafe==0.23
+Werkzeug==0.11.11
+picamera==1.8
+
+                                                         
--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, Response
 from camera import Camera
 
+
+#the rasberryPi camera module requires pi camera package so you import from camera _pi import Camera
 app = Flask(__name__)
 
 
@@ -21,4 +23,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True,threaded=True)
